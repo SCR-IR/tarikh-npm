a418c86911813983193c1c38a3f13be702c694d3
SCR-IR <gholampanahi@email.scr.ir>
2020-09-30 00:08:05 +0330

    v2.0.0 alpha

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+// import tools from './lib/tools.js';
+// import date from './lib/date.js';
+// export default {
+//     ...date,
+//     ...tools
+// }
+
+export * from './lib/tools.js';
+export * from './lib/date.js';
